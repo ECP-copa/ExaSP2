9ec920f6df4dcd11fd0d99433799a9dfc9ebe4e2
Sue Mniszewski <smm@lanl.gov>
2017-08-03 09:45:33 -0600

    Changed run script for Fermi test.

--- examples/runFermiTest.sh
+++ examples/runFermiTest.sh
@@ -1,2 +1,2 @@
-#export OMP_NUM_THREADS=16;./bin/ExaSP2-serial --hmatName data/nm.100.last.mtx --N 1900 --M 1900  --bndfil 0.63157894736842102 --beta 0.92552875303437621  --nsteps 18 --occLimit 1e-5
-export OMP_NUM_THREADS=16;./bin/ExaSP2-serial --hmatName data/poly_chain.1024.mtx --N 12288 --M 1000 --nsteps 20 --occLimit 1e-5
+export OMP_NUM_THREADS=16;./bin/ExaSP2-serial --hmatName data/nm.100.last.mtx --N 1900 --M 1900  --bndfil 0.63157894736842102 --beta 0.92552875303437621  --nsteps 18 --occLimit 1e-5
+#export OMP_NUM_THREADS=16;./bin/ExaSP2-serial --hmatName data/poly_chain.1024.mtx --N 12288 --M 1000 --nsteps 20 --occLimit 1e-5
